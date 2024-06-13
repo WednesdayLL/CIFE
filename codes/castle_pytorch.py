f7c76fce2463e321e6444f51dfc2395f51c4bafd
WednesdayLL <liuyawen1998@126.com>
2024-06-13 17:16:05 +0800

    feat:dataset

--- codes/castle_pytorch.py
+++ codes/castle_pytorch.py
@@ -1,18 +1,14 @@
 import sys
 
-sys.path.append(r"/Users/wednesday/PycharmProjects/03")
+sys.path.append(r"/CIFE")
 
 import numpy as np
 
 np.set_printoptions(suppress=True)
 import torch
 from torch import nn
-from typing import Optional
-import torch.optim as optim
-import torch.nn.functional as F
+
 from sklearn.metrics import roc_auc_score
-from .utils import showlossgraph
-from .focalLoss import focal_loss
 from .transformer import Transformer
 from .ft_transformer import FTTransformer
 from .demo_tab import TabTransformer
@@ -29,10 +25,8 @@ class CASTLE(nn.Module):
     def __init__(self, n_hidden, num_outputs, num_inputs, lr, batch_size,params,device,
                   reg_lambda=1., reg_beta=1, DAG_min=0.5, share_method='Transformer',):
         super().__init__()
-        # self.w_threshold = w_threshold
         self.DAG_min = DAG_min
         self.learning_rate = lr
-        # l2正则化
         # self.reg_lambda = reg_lambda
         # self.reg_beta = reg_beta
         self.hidden_layers = len(n_hidden)
@@ -41,9 +35,7 @@ class CASTLE(nn.Module):
         self.num_outputs = num_outputs
         self.metric = roc_auc_score
         self.share_method = share_method
-        # self.weights bias{} 字典 其中对于每个特征有自己的第一个weights,和最后一个用来分类的权重和bias
         self.weights = {}
-        # self.biases = nn.ParameterDict()
         self.hidden_mask = nn.ModuleDict()
         self.willPredict = nn.ModuleDict()
         for i in range(self.num_inputs):
@@ -52,7 +44,7 @@ class CASTLE(nn.Module):
             # ③ 预测层
             self.willPredict['out_'+str(i)] = OutUnit(n_hidden[-1], self.num_outputs,device)
 
-        # ② 共享层，可在这层进行创新
+        # ② 共享层
         # transformer
         if self.share_method == 'Transformer':
             self.share = Transformer(n_hidden)
@@ -81,33 +73,22 @@ class CASTLE(nn.Module):
         self.out_layer = {}
         self.out = {}
         # 中间层激活函数
-        # self.activation = nn.ReLU()
         self.activation = nn.GELU()
         # 预测层激活函数
         self.finalCatActivate = torch.softmax
 
     def forward(self, x):
-        # Out_0 = nn.ParameterList()
         Out_0 = []
         # for i in range(1):
         for i in range(self.num_inputs):
             # print(i)
             # h0:输入特征*mask的权重+偏置  第一个
             h0,self.weights['w_h0_'+str(i)] = self.hidden_mask['w_h0_'+str(i)](x,index=i)
-            # print('w',self.weights['w_h0_'+str(i)])
-            # 每遮盖掉一个特征，剩下的25维特征都有一个自己的第一层隐含层
             self.hidden_h0['nn_' + str(i)] = self.activation(h0)
-            # print('w', self.weights['w_h0_' + str(i)])
-            # print(self.hidden_h0['nn_' + str(i)].shape)
-
-            # if(self.share_method == 'MLP-Mixer'):
-
 
             x_i = self.share(self.hidden_h0['nn_' + str(i)])
 
             self.out_layer['out_' + str(i)] = self.willPredict['out_' + str(i)](x_i)
-            # self.out_layer['out_' + str(i)] = self.willPredict['out_' + str(i)](self.hidden_h0['nn_' + str(i)])
-
             self.out['out_' + str(i)] = self.finalCatActivate(self.out_layer['out_' + str(i)], dim=1)
             Out_0.append(self.out['out_' + str(i)][:, -1].reshape(-1, 1))
         Out = torch.cat(Out_0, dim=1)
@@ -117,7 +98,6 @@ class CASTLE(nn.Module):
         for i in range(self.num_inputs):
             self.W_0.append(torch.sqrt(torch.sum(torch.square(self.weights['w_h0_' + str(i)]), dim=1, keepdim=True)))
         self.W = torch.cat(self.W_0, dim=1)
-        # print('邻接矩阵：',self.W)
         # 邻接矩阵W，收集了所有经过激活函数的输出，要进行交叉熵的未经过激活函数的没包含label特征的输出
         return self.W,Out,self.out_layer['out_0'],self.weights,self.out['out_0']
 
@@ -126,7 +106,6 @@ class MaskUnit(nn.Module):
         super().__init__()
         self.n_hidden = n_hidden
         self.num_inputs = num_inputs
-        # self.indices = [1] * n_hidden
         self.indices = [1e-12] * n_hidden
         self.device=device
         # weight矩阵形状 num_inputs * n_hidden / n_hidden * num_inputs
@@ -168,28 +147,15 @@ class CastleLoss(nn.Module):
         self.mu = self.FeatureSelector.mu
         self.sigma = self.FeatureSelector.sigma
 
-
-        # 0️⃣ 预测函数，内部自动加softmax
-        # 交叉熵函数
-        # TODO:修改了损失函数
-        # self.loss_pre = nn.CrossEntropyLoss()
-
         # dual-focalloss函数
         self.loss_pre = DualFocalLoss()
 
 
-        # focalloss函数
-        # self.loss_pre = focal_loss(loss_alpha,loss_gamma)
-
 
     def forward(self, y, pre, X, W, weights, Out, sample=None):
-        # print('Www',W.grad)
-        # supervised_loss = self.loss_pre(pre,y.long())
-        # TODO:修改loss，增加正则项
         classification_loss = self.loss_pre(pre,y.long())
         reg = torch.mean(self.reg((self.mu + 0.5) / self.sigma))
         supervised_loss = classification_loss + self.lam * reg
-        # supervised_loss = classification_loss
 
         '''
         ① 求 Rw = (Tr(e^(WoW))-d-1)^2 部分（没平方）
@@ -198,18 +164,16 @@ class CastleLoss(nn.Module):
         '''
         d = torch.from_numpy(np.array(X.shape[1])).float().to(self.device)
         coff = 1.0
-        Z = torch.multiply(W, W)  # 相同位置的元素相乘 哈达玛积
-        # d和 dag_l 相等
+        Z = torch.multiply(W, W)
         dag_l = torch.from_numpy(np.array(X.shape[1])).float().to(self.device)
-        #  Tr(e^(WoW)
-        Z_in = torch.eye(X.shape[1],dtype=torch.float32).to(self.device)  # 对角线值为1，其余为0
-        # print(Z.dtype)
+
+        Z_in = torch.eye(X.shape[1],dtype=torch.float32).to(self.device)
+
         for i in range(1, 10):
             Z_in = torch.matmul(Z_in, Z)
             dag_l += 1. / coff * torch.trace(Z_in).to(self.device)
             coff = coff * (i + 1)
-        # dag_l = torch.trace(torch.exp(Z))
-        # 是指  Rw = (Tr(e^(WoW))-d-1)^2   DAG loss中的R 无平方（辅助loss）  所以后续用了self.h*self.h
+
         h = dag_l - d
 
         if(sample==None):
@@ -226,10 +190,9 @@ class CastleLoss(nn.Module):
             (   L=1/N|| X-f(X) ||^2   )   DAG loss中的L（重构loss）
             '''
             R = X - Out
-            # Average reconstruction loss 没用到
+
             self.average_loss = 0.5 / self.num_train * torch.sum(torch.square(R))
-            # group lasso DAG loss中的V（l1 loss）
-            # Divide the residual into untrain and train subset
+
             sample = torch.tensor(sample,dtype=torch.float32).to(device=self.device)
             _, subset_R = dynamic_partition(R.transpose(1,0), partitions=sample, num_partitions=2)
             subset_R = subset_R.transpose(1,0)
@@ -239,8 +202,6 @@ class CastleLoss(nn.Module):
             regularization_loss_subset = mse_loss_subset + self.reg_beta * L1_loss + 0.5 * self.rho * h * h + self.alpha * h
             # Add in supervised loss
             regularization_loss_subset += self.Lambda * self.rho * supervised_loss
-            # self.loss_op_dag = self.optimizer_subset.minimize(self.regularization_loss_subset)
-            # self.loss_op_supervised = self.optimizer_subset.minimize(self.supervised_loss + self.regularization_loss)
             loss_op_dag = regularization_loss_subset
             regularization_loss = 0
             loss_op_supervised = supervised_loss + regularization_loss
@@ -248,7 +209,6 @@ class CastleLoss(nn.Module):
 
 # pytorch实现
 def dynamic_partition(data, partitions, num_partitions):
-    # print(type(data),type(partitions),num_partitions)
     # Create a list of indices for each partition
     indices = [torch.nonzero(partitions == i)[:, 0] for i in range(num_partitions)]
     # Split the data tensor into a list of tensors based on the indices
--- codes/run_fea_all.py
+++ codes/run_fea_all.py
@@ -4,86 +4,15 @@ import numpy as np
 
 
 def main():
-    cancers = ['ACC', 'BLCA', 'BRCA', 'CESC', 'CHOL', 'COAD', 'DLBC',
-               'ESCA', 'GBM', 'HNSC', 'KICH', 'KIRC', 'KIRP',
-               'LGG', 'LIHC', 'LUAD', 'LUSC', 'MESO', 'OV', 'PAAD',
-               'PANCAN', 'PCPG', 'PRAD', 'READ', 'SARC', 'SKCM',
-               'STAD', 'TGCT', 'THCA', 'THYM', 'UCEC', 'UCS', 'UVM']
     cancers = ['PANCAN']
     option = ['train', 'test','casual']
-    # option = ['test','casual']
-    # option = ['test']
-    # option = [ 'embedding', 'train', 'test']
-    # option = ['embedding']
-    hidden_size1 = [30, 28, 26, 24, 22]  # 54,50,   40,30,22,18
-    hidden_size2 = [10, 8, 6]
-    # # hidden_size3 = [12,10,8,6]
-    # learning_rate = [0.0014,0.0015,0.0016,0.0017] # 0.001,0.0012,0.0013,0.0014
-    learning_rate = [
-        # 0.00141,0.00142,0.00143,0.00144,0.00145,0.00146,0.00147,
-        0.00148, 0.00149, 0.0015, 0.00151, 0.00152, 0.00153, 0.00154,
-        # 0.00155,0.00156,0.00157,0.00158,0.00159
-    ]  # 0.001,0.0012,0.0013,0.0014
-    # batch_size = [ 8]
-    dmodel = [34, 32, 30]
-    # dmodel = [40, 38, 36, ]
-    nhead = [2, 3, 4, 5, 6]
-    dimFF = [70, 68, 66, 64, 62, 60]
 
-    # scarf`
-    # embedding
-    # embeddings = [50,47,45,]
-    # embeddings = [45,43,41,39,37,]
-    embeddings = [44, 42, 40, 38, 36]
-
-    # corruption_rate=[0.5,0.6,0.7]
-    # encoder_depth = [1,2,3,4,5]
-    # head_depth = [1,2,3]
-
-    corruption_rate = [0.6, ]
-    encoder_depth = [4, ]
-    head_depth = [2]
-
-    # seeds = [i for i in range(201,601)]
-    seeds = [i for i in range(601, 1001)]
-    # print(seeds)
-
-    # for s in seeds:
-    #     for op in option:
-    #         cmd = 'python main_pytorch.py --mode %s --seed %d' % (op,s)
-    #         print(cmd)
-    #         o = check_output(cmd, shell=True, universal_newlines=True)
-    #         print(o)
     for c in cancers:
         for op in option:
             cmd = 'python main_pytorch.py --mode %s --type %s' % (op, c)
             print(cmd)
             o = check_output(cmd, shell=True, universal_newlines=True)
             print(o)
-    # for lr in learning_rate:
-    #     for op in option:
-    #         cmd = 'python main_pytorch.py --mode %s --lr %f' % (op,lr)
-    #         print(cmd)
-    #         o = check_output(cmd, shell=True, universal_newlines=True)
-    #         print(o)
-
-    # for cr in corruption_rate:
-    #         for ed in encoder_depth:
-    #             for hd in head_depth:
-    #                 for embed in embeddings:
-    #                     cmd = 'python main_pytorch.py --mode embedding --emb_dim %d --head_depth %d --corruption_rate %f --encoder_depth %d' % (embed, hd, cr, ed)
-    #                     print(cmd)
-    #                     o = check_output(cmd, shell=True, universal_newlines=True)
-    #                     print(o)
-    #                     for hz1 in hidden_size1:
-    #                         for hz2 in hidden_size2:
-    #                             for lr in learning_rate:
-    #                                 for op in option:
-    #                                     cmd = 'python main_pytorch.py --mode %s --lr %f --hz1 %d --hz2 %d --emb_dim %d --head_depth %d --corruption_rate %f --encoder_depth %d' % (op, lr, hz1, hz2, embed, hd, cr, ed)
-    #                                     print(cmd)
-    #                                     o = check_output(cmd, shell=True, universal_newlines=True)
-    #                                     print(o)
-
 
 if __name__ == "__main__":
     main()
